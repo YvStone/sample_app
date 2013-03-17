d5f1f15a89ecf7343e7370bc5dd181a6acfc44c6
Yvonne <Yvonne@localhost.(none)>
2013-03-17 14:21:22 -0600

    Finish 7.3.1 working sign up form

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "Yvonne Stone"
+    email                 "yv.stone@gmail.com"
+    password              "foobar"
+    password_confirmation "foobar"
+  end
+end
